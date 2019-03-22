42dc83d274b794925b71f0ff0c1edb76fa62e7b4
borjasolerr <borjasolrey@gmail.com>
2019-03-22 11:48:51 +0100

    added get project by id

--- index.js
+++ index.js
@@ -29,4 +29,37 @@ server.get('/api/actions', async (req, res) => {
   }
 });
 
+// GET PROJECTS AND ACTIONS BY ID
+server.get('/api/projects/:id', async (req, res) => {
+  let { id } = req.params;
+
+  try {
+    const project = await projectDB.get(id);
+
+    if (id) {
+      res.status(200).json(project);
+    } else {
+      res.status(400).json({ message: 'Please provide an ID' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to get project' });
+  }
+});
+
+server.get('/api/actions/:id', (req, res) => {
+  let { id } = req.params;
+
+  try {
+    const action = actionsDB.get(id);
+
+    if (id) {
+      res.status(200).json(action);
+    } else {
+      res.status(400).json({ message: 'Please provide an ID' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to get action' });
+  }
+});
+
 server.listen(4000);
